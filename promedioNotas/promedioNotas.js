7e0b8035fa6d591816c7af6eff1a5c4736afbb89
2Erik <erik19yogo@gmail.com>
2025-09-21 18:01:06 -0500

    Promedio notas mejorado

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -11,9 +11,17 @@ calcularPromedioNotas=function(){
     promedio=calcularPromedio(nota1,nota2,nota3);
 
     cambiarTexto("lblPromedio",promedio);
-    if(promedio>7){
-        cambiarImagen("imgNota","ok.gif");
-    }else{
+    if(promedio<5 && promedio>0){
+        cambiarTexto("lblNota","REPROBADO");
         cambiarImagen("imgNota","mal.gif");
+    }else if(promedio>=5 && promedio <=8){
+        cambiarTexto("lblNota","BUEN TRABAJO");
+        cambiarImagen("imgNota","ok.gif");
+    }else if(promedio>8 && promedio<=10){
+        cambiarTexto("lblNota","EXELENTE");
+        cambiarImagen("imgNota","exelente.gif");
+    }else{
+        cambiarTexto("lblNota","DATOS INCORRECTOS");
+        cambiarImagen("imgNota","error.gif");
     }
 }
