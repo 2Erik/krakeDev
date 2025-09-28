4367d7d2e59c7759b20dc36ff89b01f0e8b9873a
2Erik <erik19yogo@gmail.com>
2025-09-28 15:32:31 -0500

    agregado tipo vehiculo

--- placas/placas.js
+++ placas/placas.js
@@ -1,11 +1,32 @@
 validarPlaca=function(){
     let placa = recuperarTexto("txtPlaca");
     let erroresEstructura = validarEstructura(placa);
+    let vehiculo=obtenerTipoVehiculo(placa);
 
     if(erroresEstructura==null){
+        let bandera=false;
+        let provincia;
         mostrarTexto("lblResultado","ESTRUCTURA VALIDA");
+        provincia=obtenerProvincia(placa);
+
+        if(provincia!==null){
+            mostrarTexto("lblProvincia",provincia);
+            bandera=true;
+        }else{
+            mostrarTexto("lblProvincia","Provincia Incorrecta");
+        }
+
+        if(bandera===true){
+            mostrarTexto("lblVehiculo",vehiculo);
+        }else{
+            mostrarTexto("lblVehiculo","");
+        }
     }else{
         mostrarTexto("lblResultado","ESTRUCTURA INCORRECTA\n");
         mostrarTexto("lblErrores",erroresEstructura);
+        mostrarTexto("lblProvincia","")
+
     }
+
+
 }
--- placas/seviciosPlacas.js
+++ placas/seviciosPlacas.js
@@ -52,3 +52,77 @@ function validarEstructura(placa) {
     }
 }
 
+obtenerProvincia=function(placa){
+    let letra = placa.charCodeAt(0);
+
+    if (letra === 65) {
+        return "Azuay";
+    } else if (letra === 66) {
+        return "Bolivar";
+    } else if (letra === 67) {
+        return "Carchi";
+    } else if (letra === 69) {
+        return "Esmeraldas";
+    } else if (letra === 71) {
+        return "Guayas";
+    } else if (letra === 72) {
+        return "Chimborazo";
+    } else if (letra === 73) {
+        return "Imbabura";
+    } else if (letra === 74) {
+        return "Santo Domingo de los Tsachilas";
+    } else if (letra === 75) {
+        return "Sucumbios";
+    } else if (letra === 76) {
+        return "Loja";
+    } else if (letra === 77) {
+        return "Manabi";
+    } else if (letra === 78) {
+        return "Napo";
+    } else if (letra === 79) {
+        return "El Oro";
+    } else if (letra === 80) {
+        return "Pichincha";
+    } else if (letra === 81) {
+        return "Orellana";
+    } else if (letra === 82) {
+        return "Los Rios";
+    } else if (letra === 83) {
+        return "Pastaza";
+    } else if (letra === 84) {
+        return "Tungurahua";
+    } else if (letra === 85) {
+        return "Canar";
+    } else if (letra === 86) {
+        return "Morona Santiago";
+    } else if (letra === 87) {
+        return "Galapagos";
+    } else if (letra === 88) {
+        return "Cotopaxi";
+    } else if (letra === 89) {
+        return "Santa Elena";
+    } else if (letra === 90) {
+        return "Zamora Chinchipe";
+    } else {
+        return null;
+    }
+}
+
+obtenerTipoVehiculo=function(placa){
+    let tipoVehiculo=placa.charCodeAt(1);
+
+    if(tipoVehiculo===65 || tipoVehiculo===90){
+        return "Vehiculo Comercial";
+    }else if(tipoVehiculo===69){
+        return "Vehiculo Gubernamental";
+    }else if(tipoVehiculo===88){
+        return "Vehiculo de uso oficial";
+    }else if(tipoVehiculo===83){
+        return "Vehiculo del gobierno provincial";
+    }else if(tipoVehiculo===77){
+        return "Vehiculo municipales";
+    }else{
+        return "Vehiculo particular";
+    }
+}
+
