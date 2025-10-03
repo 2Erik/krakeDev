3bee1fa77967107b8aaea6e6fadec7adf6135989
2Erik <erik19yogo@gmail.com>
2025-10-02 21:02:43 -0500

    Creado funcion guardarPalabra

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,9 +1,31 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta="";
 esMayuscula=function(letra){
     let mayuscula=letra.charCodeAt(0);
     if(mayuscula>=65 && mayuscula<=90){
         return true;
     }else{
         return false;
+    }
+}
+
+guardarPalabra=function(){
+    let palabra=recuperarTexto("txtSecreta");
+    let caracter;
+    if(palabra.length===5){
+        palabraSecreta="";
+        for(let i=0;i<palabra.length;i++){
+            caracter=palabra.charAt(i);
+            if(esMayuscula(caracter)){
+                palabraSecreta += caracter;
+            }else{
+                alert("La palabra debe ser Mayuscula y con 5 letras.");
+                palabraSecreta="";
+                break;
+            }
+        }
+        console.log(palabraSecreta);
+    }else{
+        alert("La palabra debe ser Mayuscula y con 5 letras.");
     }
 }
