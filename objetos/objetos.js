bd79340fd0baec791e3d8aff84b8b6fa3bf129de
2Erik <erik19yogo@gmail.com>
2025-10-07 21:30:23 -0500

    funciones Ingresar Buscar Modificar

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -38,3 +38,57 @@ crearProducto=function(){
         console.log("Producto 2 tiene mayor stock");
     }
 }
+
+modificarAtributos=function(){
+    let cuenta={
+        numero:"54255458",
+        saldo:0.0
+    }
+    cuenta.saldo=100;
+    cuenta.saldo+=10;
+    console.log(cuenta.saldo);
+}
+
+crearCliente=function(){
+    let cliente={
+        cedula:"1805050824",
+        nombre:"Armando"
+    }
+    let cliente1={};
+    cliente1.nombre="Salaz";
+    cliente1.apellido="Paredes";
+    cliente1.cedula="18563255"
+}
+
+probarIncrementoSaldo=function(){
+    let cta={
+        numero:"1215215",
+        saldo:34.0
+    }
+    incrementarSaldo(cta,100);
+    console.log(cta.saldo);
+}
+
+incrementarSaldo=function(cuenta,monto){
+    cuenta.saldo+=monto;
+}
+
+probarDeterminarMayor=function(){
+    let per1={nombre:"Juan",edad:89};
+    let per2={nombre:"Pepe",edad:50};
+    let mayor;
+    mayor=determinarMayor(per1,per2);
+    if(mayor != null){
+        console.log("El mayor es: "+mayor.nombre);
+    }
+}
+
+determinarMayor=function(persona1,persona2){
+    if(persona1.edad>persona2.edad){
+        return persona1;
+    }else if(persona2.edad>persona1.edad){
+        return persona2;
+    }else{
+        return null;
+    }
+}
