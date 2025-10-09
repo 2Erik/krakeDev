b6c734b49dc3b7b43285ed0fdb9a636afbe8b555
2Erik <erik19yogo@gmail.com>
2025-10-08 23:10:40 -0500

    Funcion mostraEmpleados

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,34 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"0594105087",nombre:"Armando",apellido:"Paredes",sueldo:600.0}
 ];
+
+mostrarEmpleados=function(){
+    let cmpTabla=document.getElementById("tablaEmpleados");
+    let componentesEmpleado;
+    let tabla="<table><tr>"+
+    "<th>CEDULA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>APELLIDO</th>"+
+    "<th>SUELDO</th>"+"</tr>";
+
+    for(let i=0;i<empleados.length;i++){
+        componentesEmpleado=empleados[i];
+        tabla+="<tr><td>"+componentesEmpleado.cedula+"</td>"
+        +"<td>"+componentesEmpleado.nombre+"</td>"
+        +"<td>"+componentesEmpleado.apellido+"</td>"
+        +"<td>"+componentesEmpleado.sueldo+"</td></tr>";
+    }
+    tabla+="</table>";
+    cmpTabla.innerHTML=tabla;
+}
 
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol=function(){
